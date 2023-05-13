aadadef80ce50ec04d5487a90c12d1ca4c0d04bc
Victor Alcântara <victorjnio75@gmail.com>
2023-05-13 14:40:58 -0300

    todo app v1

---
+++ database.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        id: "12412",
+        text: "Lavar o carro",
+        done: false,
+        priority: "high"
+    },
+    {
+        id: "55124",
+        text: "Fazer compras",
+        done: false,
+        priority: "medium"
+    }
+]
+
+
+export default data;
---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 800,
+    // resizable: false
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
